120a2bcdc2f68694897ae2d3968897874adb8fc5
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-04-02 20:51:53 +0300

    Попытка добавления регистрации

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,16 @@
 from flask import *
 from parse import *
+from flask_login import *
+from data.users import *
+from data import db_session
+from form import *
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 parse_app = ParseApp()
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/DATA_authors_and_titles', methods=['POST'])
@@ -37,6 +43,38 @@ def final_result_load():
     text = req['text']
     response_result = parse_app.get_correct_text(audio_repeat, text)
     return response_result
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            # about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        # return redirect('/login')
+        return redirect('/success')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
