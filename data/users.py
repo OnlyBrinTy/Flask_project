bbd3a9347dec786622f734028c4a4cab223ad7d1
Roma <onlybrinty@gmail.com>
2023-04-11 13:56:50 +0200

    little fix

--- data/users.py
+++ data/users.py
@@ -3,7 +3,6 @@ import sqlalchemy
 from flask_login import UserMixin
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from .db_session import SqlAlchemyBase
 
 
