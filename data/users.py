e4bc1b3e8c7747ae23a587897570f38d90f119b8
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-04-13 22:24:59 +0300

    регистрация

--- data/users.py
+++ data/users.py
@@ -14,16 +14,11 @@ class User(SqlAlchemyBase, UserMixin):
                            primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    jobs = orm.relationship("Jobs", back_populates='user')
 
     def __repr__(self):
         return f'<Colonist> {self.id} {self.surname} {self.name}'
