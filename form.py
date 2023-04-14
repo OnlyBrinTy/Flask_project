7b9a28c4934b0105a05220e4f6c87b7a11ac9ddb
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-04-14 19:56:34 +0300

    корректировка регистрации

--- form.py
+++ form.py
@@ -8,7 +8,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
-    submit = SubmitField('Войти')
+    submit = SubmitField('Зарегистрироваться')
 
 
 class LoginForm(FlaskForm):
