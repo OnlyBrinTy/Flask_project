63c1e679e1eb2a1ce2c64d6c5680ac4893328da6
PitchBlackMilk <m.denisov16.01@gmail.com>
2023-04-18 09:10:24 +0300

    mask beginning

--- data/paragraph.py
+++ data/paragraph.py
@@ -8,8 +8,8 @@ class Paragraph(SqlAlchemyBase):
     id = Column(Integer(), autoincrement=True, primary_key=True)
     article_id = Column(Integer(), ForeignKey("articles.id"), nullable=False)
     num = Column(Integer(), nullable=False)
-
     text = Column(String(), nullable=False, index=True)
+    is_read = Column(Integer(), nullable=True, default=0)
 
     def __str__(self):
         return self.text
